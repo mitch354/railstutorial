cced43978dc31f9c40fcf66f670e9788ce832f0c
Mitchell Hole <mhole@sfu.ca>
2017-01-23 19:15:51 -0800

    Micropost resource added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123060010) do
+ActiveRecord::Schema.define(version: 20170124030306) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
